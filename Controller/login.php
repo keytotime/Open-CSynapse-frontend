680ab6acf689a262a2c0444df2b3c65a8e689b0d
N-Thomas <kamakazininja@gmail.com>
2016-09-19 13:57:56 -0600

    updated login cookie extraction

--- Controller/login.php
+++ Controller/login.php
@@ -3,10 +3,20 @@
 if (isset($_POST['username']) && isset($_POST['password'])){
     echo("variables set<br><br>");
     $url = "http://api:8888/login?username=" . $_POST['username'] . "&password=" . $_POST['password'];
-    $json = file_get_contents($url);
-    $allobj = json_decode($json);
-    var_dump($json);
-    var_dump($allobj);
+    $ch = curl_init($url);
+    $cookie_file = tempnam("/tmp", "user_cookie");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
+    curl_setopt($ch, CURLOPT_POST, true);
+    $data = curl_exec($ch);
+    $cookie_list = curl_getinfo($ch, CURLINFO_COOKIELIST);
+    var_dump($cookie_list);
+    echo("<br />");
+    curl_close($ch);
+    #$allobj = json_decode($json);
+    var_dump($data);
+    #set cookie on user's side
+    #var_dump($allobj);
     echo("<br><br>extraction finished");
 }
 
