a7e8516e5e1bde74823e0c927f1921e68827a88c
N-Thomas <nick.thomas@utah.edu>
2016-09-18 19:24:45 -0600

    added verbosity for debugging

--- Controller/login.php
+++ Controller/login.php
@@ -1,11 +1,12 @@
 <?php
 
 if (isset($_POST['username']) && isset($_POST['password'])){
+    echo("variables set");
     $url = "api:8888/login?username=" . $_POST['username'] . "&password=" . $_POST['password'];
-        $json = file_get_contents($url);
-        $allobj = json_decode($json);
-
-        echo($allobj);
+    $json = file_get_contents($url);
+    $allobj = json_decode($json);
+    echo($json);
+    echo($allobj);
 }
 
 else{
