ae32f2bbeca06e20396a48f780a70b48f1f0c8f5
N-Thomas <nick.thomas@utah.edu>
2016-09-18 19:34:52 -0600

    added http to api url

--- Controller/login.php
+++ Controller/login.php
@@ -1,13 +1,13 @@
 <?php
 
 if (isset($_POST['username']) && isset($_POST['password'])){
-    echo("variables set");
-    $url = "api:8888/login?username=" . $_POST['username'] . "&password=" . $_POST['password'];
+    echo("variables set<br><br>");
+    $url = "http://api:8888/login?username=" . $_POST['username'] . "&password=" . $_POST['password'];
     $json = file_get_contents($url);
     $allobj = json_decode($json);
     var_dump($json);
     var_dump($allobj);
-    echo("\n\nextraction finished");
+    echo("<br><br>extraction finished");
 }
 
 else{
