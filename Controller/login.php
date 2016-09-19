d1f2d6054dba1ef04a13f412962a6fef6cbe4184
N-Thomas <nick.thomas@utah.edu>
2016-09-18 19:29:48 -0600

    obj dumps

--- Controller/login.php
+++ Controller/login.php
@@ -5,9 +5,9 @@ if (isset($_POST['username']) && isset($_POST['password'])){
     $url = "api:8888/login?username=" . $_POST['username'] . "&password=" . $_POST['password'];
     $json = file_get_contents($url);
     $allobj = json_decode($json);
-    echo($json);
-    echo($allobj);
-    echo("extraction finished");
+    var_dump($json);
+    var_dump($allobj);
+    echo("\n\nextraction finished");
 }
 
 else{
