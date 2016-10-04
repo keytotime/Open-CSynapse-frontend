bfa433744916adc2f8d25410eb458201ce49bfda
N-Thomas <nick.thomas@utah.edu>
2016-10-03 21:49:56 -0600

    ripped out more dummy data

--- View/nav.php
+++ View/nav.php
@@ -33,151 +33,153 @@ $nav = '<nav class="navbar navbar-default navbar-static-top" role="navigation" s
                                 <div>
                                     <strong>CSynapse</strong>
                                     <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
+                                        <em>10/3/2016</em>
                                     </span>
                                 </div>
-                                <div>Server Maintenance Scheduled in 2 days</div>
+                                <div>Thank you for participating in the CSynapse alpha. We appriciate your help and feedback.</div>
                             </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <strong>CSynapse</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                                </div>
-                                <div>Message Recieved by Admin</div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a class="text-center" href="#">
-                                <strong>Read All Messages</strong>
-                                <i class="fa fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
+                        </li>'.
+                        // <li class="divider"></li>
+                        // <li>
+                        //     <a href="#">
+                        //         <div>
+                        //             <strong>CSynapse</strong>
+                        //             <span class="pull-right text-muted">
+                        //                 <em>Yesterday</em>
+                        //             </span>
+                        //         </div>
+                        //         <div>Message Recieved by Admin</div>
+                        //     </a>
+                        // </li>
+                        // <li class="divider"></li>
+                        // <li>
+                        //     <a class="text-center" href="#">
+                        //         <strong>Read All Messages</strong>
+                        //         <i class="fa fa-angle-right"></i>
+                        //     </a>
+                        // </li>
+                    '</ul>
                     <!-- /.dropdown-messages -->
                 </li>
                 <!-- /.dropdown -->
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-tasks fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-tasks">
-                        <li>
-                             <a href="#">
-                                 <div>
-                                     <p>
-                                        <strong>Weather Patterns: Texas</strong>
-                                        <span class="pull-right text-muted">40% Complete</span>
-                                    </p>
-                                    <div class="progress progress-striped active">
-                                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                            <span class="sr-only">40% Complete</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <p>
-                                        <strong>Traffic Congestion</strong>
-                                        <span class="pull-right text-muted">20% Complete</span>
-                                    </p>
-                                    <div class="progress progress-striped active">
-                                       <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-                                           <span class="sr-only">20% Complete</span>
-                                       </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-center" href="/index.php">
-                                <strong>See All Training Progress</strong>
-                                <i class="fa fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-tasks -->
-                </li>
+                    </a>'.
+                    // <ul class="dropdown-menu dropdown-tasks">
+                    //     <li>
+                    //          <a href="#">
+                    //              <div>
+                    //                  <p>
+                    //                     <strong>Weather Patterns: Texas</strong>
+                    //                     <span class="pull-right text-muted">40% Complete</span>
+                    //                 </p>
+                    //                 <div class="progress progress-striped active">
+                    //                     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                    //                         <span class="sr-only">40% Complete</span>
+                    //                     </div>
+                    //                 </div>
+                    //             </div>
+                    //         </a>
+                    //     </li>
+                    //     <li class="divider"></li>
+                    //     <li>
+                    //         <a href="#">
+                    //             <div>
+                    //                 <p>
+                    //                     <strong>Traffic Congestion</strong>
+                    //                     <span class="pull-right text-muted">20% Complete</span>
+                    //                 </p>
+                    //                 <div class="progress progress-striped active">
+                    //                    <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                    //                        <span class="sr-only">20% Complete</span>
+                    //                    </div>
+                    //                 </div>
+                    //             </div>
+                    //         </a>
+                    //     </li>
+                    //     <li>
+                    //         <a class="text-center" href="/index.php">
+                    //             <strong>See All Training Progress</strong>
+                    //             <i class="fa fa-angle-right"></i>
+                    //         </a>
+                    //     </li>
+                    // </ul>
+                    // <!-- /.dropdown-tasks -->
+                '</li>
                 <!-- /.dropdown -->
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-bell fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-alerts">
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="glyphicon glyphicon-ok"></i> Android Malware Detection Completed Training
-                                    <span class="pull-right text-muted small">4 minutes ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="glyphicon glyphicon-cog"></i> Traffic Congestion Training Started
-                                    <span class="pull-right text-muted small">  4 Hours ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="glyphicon glyphicon-ok"></i> Traffic Congestion CSynapse Created
-                                    <span class="pull-right text-muted small">4 Hours ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="glyphicon glyphicon-cog"></i> Weather Patterns: Texas Training Started
-                                    <span class="pull-right text-muted small"> 6 Hours Ago </span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="glyphicon glyphicon-ok"></i> Weather Patterns: Texas CSynapse Created
-                                    <span class="pull-right text-muted small"> 7 Hours Ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a class="text-center" href="/index.php">
-                                <strong>See All Alerts</strong>
-                                <i class="fa fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-alerts -->
-                </li>
+                    </a>'
+                    // <ul class="dropdown-menu dropdown-alerts">
+                    //     <li>
+                    //         <a href="#">
+                    //             <div>
+                    //                 <i class="glyphicon glyphicon-ok"></i> Android Malware Detection Completed Training
+                    //                 <span class="pull-right text-muted small">4 minutes ago</span>
+                    //             </div>
+                    //         </a>
+                    //     </li>
+                    //     <li class="divider"></li>
+                    //     <li>
+                    //         <a href="#">
+                    //             <div>
+                    //                 <i class="glyphicon glyphicon-cog"></i> Traffic Congestion Training Started
+                    //                 <span class="pull-right text-muted small">  4 Hours ago</span>
+                    //             </div>
+                    //         </a>
+                    //     </li>
+                    //     <li class="divider"></li>
+                    //     <li>
+                    //         <a href="#">
+                    //             <div>
+                    //                 <i class="glyphicon glyphicon-ok"></i> Traffic Congestion CSynapse Created
+                    //                 <span class="pull-right text-muted small">4 Hours ago</span>
+                    //             </div>
+                    //         </a>
+                    //     </li>
+                    //     <li class="divider"></li>
+                    //     <li>
+                    //         <a href="#">
+                    //             <div>
+                    //                 <i class="glyphicon glyphicon-cog"></i> Weather Patterns: Texas Training Started
+                    //                 <span class="pull-right text-muted small"> 6 Hours Ago </span>
+                    //             </div>
+                    //         </a>
+                    //     </li>
+                    //     <li class="divider"></li>
+                    //     <li>
+                    //         <a href="#">
+                    //             <div>
+                    //                 <i class="glyphicon glyphicon-ok"></i> Weather Patterns: Texas CSynapse Created
+                    //                 <span class="pull-right text-muted small"> 7 Hours Ago</span>
+                    //             </div>
+                    //         </a>
+                    //     </li>
+                    //     <li class="divider"></li>
+                    //     <li>
+                    //         <a class="text-center" href="/index.php">
+                    //             <strong>See All Alerts</strong>
+                    //             <i class="fa fa-angle-right"></i>
+                    //         </a>
+                    //     </li>
+                    // </ul>
+                    // <!-- /.dropdown-alerts -->
+
+                .'</li>
                 <!-- /.dropdown -->
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
+                    <ul class="dropdown-menu dropdown-user">'
+                        // <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        // </li>
+                        // <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        // </li>
+                        // <li class="divider"></li>
+                        .'
                         <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                         </li>
                     </ul>
@@ -189,19 +191,20 @@ $nav = '<nav class="navbar navbar-default navbar-static-top" role="navigation" s
 
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-default" type="button">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
-                        <li>
+                    <ul class="nav" id="side-menu">'
+                        // <li class="sidebar-search">
+                        //     <div class="input-group custom-search-form">
+                        //         <input type="text" class="form-control" placeholder="Search...">
+                        //         <span class="input-group-btn">
+                        //             <button class="btn btn-default" type="button">
+                        //                 <i class="fa fa-search"></i>
+                        //             </button>
+                        //         </span>
+                        //     </div>
+                        //     <!-- /input-group -->
+                        // </li>
+
+                    .'    <li>
                             <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
                         <li>
