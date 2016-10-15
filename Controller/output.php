c59db69578ef0a7d6be209635d7826c5411c01d5
N-Thomas <nick.thomas@utah.edu>
2016-10-14 23:08:45 -0600

    added stubs for needed pages

---
+++ Controller/output.php
@@ -0,0 +1,18 @@
+<?php
+/* Nick Thomas and Andrew Stoddard
+University of Utah
+cs4000 - Capstone
+4/17/2016
+CSynapse
+*/
+
+require '../Model/hidden/api.php';
+require '../Controller/api_request_functions.php';
+
+
+
+require '../View/head.php';
+require '../View/nav.php';
+require '../View/output.php';
+
+?>
---
+++ View/output.php
@@ -0,0 +1,9 @@
+<?php
+/* Nick Thomas and Andrew Stoddard
+University of Utah
+cs4000 - Capstone
+4/17/2016
+CSynapse
+*/
+
+?>
