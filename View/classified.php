9c6b4ed469181ee795fd48db0db33a1c2f8297b9
Nick Thomas <kamakazininja@gmail.com>
2016-12-08 22:42:27 -0700

    classified view addition

---
+++ View/classified.php
@@ -0,0 +1,105 @@
+<?php
+/* Nick Thomas and Andrew Stoddard
+University of Utah
+cs4000 - Capstone
+4/17/2016
+CSynapse
+*/
+
+echo $head . '
+
+<body>
+
+    <div id="wrapper">
+
+        ' . $nav . '
+        <!-- Page Content -->
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">'.$name.'</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Classification Results
+                            <div id="multi-dimension" class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                                        Download
+                                        <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu pull-right" role="menu">
+                                        <li><a href="download.php?id=' . $_GET['id'] . '&name=' . urlencode($_GET['name']) . '&ext=csv">CSV</a>
+                                        </li>
+                                        <li><a href="download.php?id=' . $mongoId . '&name=' . urlencode($_GET['name']) . '&ext=txt">Text</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <table width="100%" class="table table-striped table-bordered table-hover" id="classifications">
+                                <thead>
+                                    '.$table_header.'
+                                </thead>
+                                <tbody>'.$table.'</tbody>
+                            </table>
+
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+
+    </div>
+    <!-- /#wrapper -->
+
+
+
+    <!-- jQuery -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+        <!-- Tables  -->
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs/jqc-1.12.4/dt-1.10.13/datatables.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+    
+
+
+
+
+    <script>
+        $(document).ready(function() {
+            $(\'#classifications\').DataTable( {
+                "responsive": true
+            });
+        } );
+    </script>
+
+
+
+
+   
+
+
+</body>
+
+</html>';
+
+?>
